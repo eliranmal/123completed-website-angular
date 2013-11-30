577488809a9f02aee7e55b49d0ba78b9ead22bc6
eliranm <eliranmal@gmail.com>
2013-11-30 22:24:52 +0200

    images added, scaffold created, routing configured

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,22 @@
 'use strict';
 
-angular.module('123CompletedWebsiteApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('123CompletedWebsiteApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/content/:page', {
+                templateUrl: function (params) {
+                    return 'views/content/' + params['page'];
+                },
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }])
+    .run(['$route', function ($route) {
+        $route.reload();
+    }]);
